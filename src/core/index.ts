8976f53e4034738ba11058f8576063bf65368e02
KeJunMao <kejun1997@gmail.com>
2023-09-28 16:54:06 +0800

    feat: export core

---
+++ src/core/index.ts
@@ -0,0 +1,4 @@
+export * from "./context"
+export * from "./directive"
+export * from "./directives"
+export * from "./unplugin"
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
 export * from './types'
-export * from './core/directive'
-export { default } from './core/unplugin'
-export * from './core/directives'
+export * from './core'
