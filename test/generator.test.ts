9062c4456678a4ad8f2a328505291e6f62b81ccd
kejun <kejun1997@gmail.com>
2024-01-21 19:23:18 +0800

    test: update

--- test/generator.test.ts
+++ test/generator.test.ts
@@ -1,5 +1,8 @@
+import { readFileSync } from 'node:fs'
+import { resolve } from 'node:path'
 import { describe, expect, it } from 'vitest'
 import { Generator } from '../src/core/context/generator'
+import { Context, Lexer, Parser } from '../src'
 
 describe('generator', () => {
   it('should generate code for Program node', () => {
@@ -21,6 +24,15 @@ describe('generator', () => {
     }
     const result = Generator.generate(node)
     expect(result).toBe('console.log("Hello, World!");')
+  })
+
+  it('should generate code without transform', () => {
+    const ctx = new Context()
+    const code = readFileSync(resolve(__dirname, './fixtures/if.html'), 'utf-8')
+    const tokens = Lexer.lex(code, ctx.lexers)
+    const ast = Parser.parse(tokens, ctx.parsers)
+    const generated = Generator.generate(ast, ctx.generates)
+    expect(generated.replaceAll(/\s/g, '')).toBe(code.replaceAll(/\s/g, ''))
   })
 
   it('should throw an error for unknown node type', () => {
